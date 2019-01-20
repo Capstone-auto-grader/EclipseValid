71e807ad95103006fa3b94604ddfd0ab42a02b5b
Ari Carr <acarr@brandeis.edu>
2019-01-20 11:44:24 -0500

    Form and result text adjustments

--- app/controllers/validator_controller.rb
+++ app/controllers/validator_controller.rb
@@ -3,7 +3,7 @@ include ValidatorHelper
 class ValidatorController < ApplicationController
 
   def home
-    @zip_status = validate params[:zip_file][:file] unless params[:zip_file].nil?
+    @zip_status = validate(params[:zip_file][:file]).html_safe unless params[:zip_file].nil?
   end
 
 end
